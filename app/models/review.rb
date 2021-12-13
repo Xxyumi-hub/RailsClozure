682490c775ee2e308ab5d1814b45aa8ed65dd9ea
Grace <jequel07@gmail.com>
2021-12-12 21:00:27 -0500

    added validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :store
+  validates :content, :name, presence: true
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,4 @@
 class Store < ApplicationRecord
     has_many :reviews
+    validates :name, :address, presence: true
 end
